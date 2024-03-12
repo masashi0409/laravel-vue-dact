8d50718b039a7a4c0c76c6369d814bd8c7313c4e
shimano masashi <masashi.shimano0409@gmail.com>
2024-03-12 11:36:15 +0900

    Vuetifyインストール

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 100000000,
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: "resources/js/app.js",
             refresh: true,
         }),
         vue({
